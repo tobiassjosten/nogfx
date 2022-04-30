733f344d07a2f902c2661a99383a952147bb18cb
Tobias Sjösten <tobias@tobiassjosten.net>
2022-04-30 11:33:51 +0200

    Add simpex package

---
+++ pkg/simpex/pattern_test.go
@@ -0,0 +1,172 @@
+package simpex_test
+
+import (
+	"testing"
+
+	"github.com/tobiassjosten/nogfx/pkg/simpex"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMatch(t *testing.T) {
+	tcs := map[string]struct {
+		pattern []byte
+		text    []byte
+		matches [][]byte
+	}{
+		"exact match simple": {
+			pattern: []byte("Lorem ipsum dolor sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"exact match capture": {
+			pattern: []byte("{Lorem} ipsum dolor sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{[]byte("Lorem")},
+		},
+		"exact match escaped capture simple": {
+			pattern: []byte("{{Lorem}} ipsum dolor sit amet."),
+			text:    []byte("{Lorem} ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"exact match escaped capture capture one": {
+			pattern: []byte("{{{Lorem}}} ipsum dolor sit amet."),
+			text:    []byte("{Lorem} ipsum dolor sit amet."),
+			matches: [][]byte{[]byte("{Lorem}")},
+		},
+		"exact match escaped capture capture two": {
+			pattern: []byte("{{{Lorem}} ipsum} dolor sit amet."),
+			text:    []byte("{Lorem} ipsum dolor sit amet."),
+			matches: [][]byte{[]byte("{Lorem} ipsum")},
+		},
+
+		"character match simple": {
+			pattern: []byte("Lorem ipsum do?or sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"character match capture": {
+			pattern: []byte("Lorem ipsum do{?}or sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{[]byte{'l'}},
+		},
+		"character match escaped one": {
+			pattern: []byte("Lorem ipsum do??or sit amet."),
+			text:    []byte("Lorem ipsum do?or sit amet."),
+			matches: [][]byte{},
+		},
+		"character match escaped two": {
+			pattern: []byte("Lorem ipsum do???or sit amet."),
+			text:    []byte("Lorem ipsum do?lor sit amet."),
+			matches: [][]byte{},
+		},
+
+		"word match simple": {
+			pattern: []byte("Lorem ^ dolor sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"word match capture": {
+			pattern: []byte("Lorem {^} dolor sit amet."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{[]byte("ipsum")},
+		},
+		"word match escaped one": {
+			pattern: []byte("Lorem ^^ dolor sit amet."),
+			text:    []byte("Lorem ^ dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"word match escaped two": {
+			pattern: []byte("Lorem ^^^ dolor sit amet."),
+			text:    []byte("Lorem ^ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+
+		"phrase match simple one": {
+			pattern: []byte("Lorem ipsum dolor *."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"phrase match simple two": {
+			pattern: []byte("Lorem ipsum dolor * lol."),
+			text:    []byte("Lorem ipsum dolor sit amet lol."),
+			matches: [][]byte{},
+		},
+		"phrase match capture one": {
+			pattern: []byte("Lorem ipsum dolor {*}."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{[]byte("sit amet")},
+		},
+		"phrase match capture two": {
+			pattern: []byte("Lorem ipsum dolor {*} lol."),
+			text:    []byte("Lorem ipsum dolor sit amet lol."),
+			matches: [][]byte{[]byte("sit amet")},
+		},
+		"phrase match escaped one": {
+			pattern: []byte("Lorem ipsum dolor **."),
+			text:    []byte("Lorem ipsum dolor *."),
+			matches: [][]byte{},
+		},
+		"phrase match escaped two": {
+			pattern: []byte("Lorem ipsum dolor ***."),
+			text:    []byte("Lorem ipsum dolor *sit amet."),
+			matches: [][]byte{},
+		},
+
+		"combination match simple": {
+			pattern: []byte("Lorem ^ do?or *."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{},
+		},
+		"combination match escaped": {
+			pattern: []byte("Lorem ^^ do??or **."),
+			text:    []byte("Lorem ^ do?or *."),
+			matches: [][]byte{},
+		},
+		"combination match capture": {
+			pattern: []byte("{Lorem} {^} do{?}or {*}."),
+			text:    []byte("Lorem ipsum dolor sit amet."),
+			matches: [][]byte{
+				[]byte("Lorem"),
+				[]byte("ipsum"),
+				[]byte{'l'},
+				[]byte("sit amet"),
+			},
+		},
+		"combination match capture escaped": {
+			pattern: []byte("{{{Lorem}}} {^^} do{??}or {**}."),
+			text:    []byte("{Lorem} ^ do?or *."),
+			matches: [][]byte{
+				[]byte("{Lorem}"),
+				[]byte("^"),
+				[]byte{'?'},
+				[]byte("*"),
+			},
+		},
+
+		"learn fitness from maric begin": {
+			pattern: []byte("* bows to you and commences the lesson in ^."),
+			text:    []byte("Maric, a filthy ratman bows to you and commences the lesson in Fitness."),
+			matches: [][]byte{},
+		},
+		"learn fitness from maric continue": {
+			pattern: []byte("* continues your training in ^."),
+			text:    []byte("Maric, a filthy ratman continues your training in Fitness."),
+			matches: [][]byte{},
+		},
+		"learn fitness from maric finish": {
+			pattern: []byte("* bows to you - the lesson in ^ is over."),
+			text:    []byte("Maric, a filthy ratman bows to you - the lesson in Fitness is over."),
+			matches: [][]byte{},
+		},
+	}
+
+	for name, tc := range tcs {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.matches, simpex.Match(
+				tc.pattern,
+				tc.text,
+			))
+		})
+	}
+}
